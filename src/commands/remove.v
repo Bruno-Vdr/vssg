3298a89d415e3688b0b810e332a724ebe8fe8d21
Bruno <bruno@asterope.fr>
2025-01-26 08:58:01 +0100

    Finished remove command, added -f option to force directory deletion.

--- src/commands/remove.v
+++ src/commands/remove.v
@@ -24,7 +24,7 @@ pub fn Remove.new() Command {
 		desc:    'Remove an entry/push from a topic.'
 		help:    Remove.help()
 		arg_min: 1
-		arg_max: 1
+		arg_max: 2
 		exec:    remove
 	}
 }
@@ -32,7 +32,7 @@ pub fn Remove.new() Command {
 // help give a complete description of the command, including parameters.
 fn Remove.help() string {
 	return '
-Command: ${term.green('vssg')} ${term.yellow('remove')} ${term.magenta('id')}
+Command: ${term.green('vssg')} ${term.yellow('remove')} ${term.magenta('id')} [-f]
 
 ${term.red('Warning:')} This command must be launched from within topic directory.
 To get the push\'s ${term.magenta('id')}, just do "${term.green('vssg')} ${term.yellow('show')}"
@@ -42,15 +42,25 @@ The remove command deletes a push from a topic:
 	-Print out command to delete remaining directories.
 
 Note: the remove commands only remove push from ${cst.topic_file}. Directory ${cst.push_dir_prefix} is
-      not delete neither its contained files, subdirectory and images, its suffixed with ${cst.push_dir_removed_suffix}.
+      not delete neither its contained files, subdirectory and images, its suffixed with ${cst.push_dir_removed_suffix}
+      UNLESS ${term.red('-f')} is passed on the command line.
 '
 }
 
 // remove command feature are implemented here. The parameters number has been checked before call.
 fn remove(param []string) ! {
+	mut force_delete := false
 	id := strconv.parse_uint(param[0], 10, 64) or {
-		return error('Cannot convert "${param[0]}" to unsigned ID.')
+		return error('Cannot convert "${param[0]}" to unsigned ID. ${@FILE_LINE}')
 	} // int
+
+	if param.len == 2 {
+		if param[1] == '-f' {
+			force_delete = true
+		} else {
+			return error('Unknown parameter "${param[1]}". ${@FILE_LINE}')
+		}
+	}
 
 	// Load .topic
 	mut topics := Topic.load()!
@@ -60,12 +70,17 @@ fn remove(param []string) ! {
 		topics.posts.delete(id)
 		topics.save('./')!
 
-		// Move push directory to __directory
-		os.mv(dir, '${dir}${cst.push_dir_removed_suffix}') or {
-			return error('Unable to mv ${dir} to ${dir}${cst.push_dir_removed_suffix} !')
+		if force_delete {
+			os.rmdir_all(dir) or { return error('Unable to rmdir ${dir}. ${err}. ${@FILE_LINE}.') }
+			println('Removed ${term.blue(dir)} directory.')
+		} else {
+			// Move push directory to __directory
+			os.mv(dir, '${dir}${cst.push_dir_removed_suffix}') or {
+				return error('Unable to mv ${dir} to ${dir}${cst.push_dir_removed_suffix}. ${err}. ${@FILE_LINE}.')
+			}
+			println('renaming push directory ${term.blue('${dir}')} to ${term.blue('${dir}${cst.push_dir_removed_suffix}')}')
+			println('You can remove all attached push data by doing "rm -rf ${dir}${cst.push_dir_removed_suffix}"}')
 		}
-		println('renaming push directory ${term.blue('${dir}')} to ${term.blue('${dir}${cst.push_dir_removed_suffix}')}')
-		println('You can remove all attached push data by doing "rm -rf ${dir}${cst.push_dir_removed_suffix}"}')
 
 		// Rebuilt HTML topic list
 		topics.generate_pushes_list_html()!
