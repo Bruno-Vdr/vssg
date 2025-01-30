3137b61e8618c6cb90b99955ecf20ad7ab4a83f6
Bruno <bruno@asterope.fr>
2025-01-30 01:39:19 +0100

    Typo on comment.

--- src/commands/command.v
+++ src/commands/command.v
@@ -1,12 +1,12 @@
 module commands
 
 pub interface Command {
-	name    string // Command name as used on CLI
-	desc    string // Single line description
-	help    string // Detailed and formated description
-	arg_min int    // Minimal argument number expected
-	arg_max int    // Maximal argument number expected
-	exec    fn (s []string) !Command callback.
+	name    string            // Command name as used on CLI
+	desc    string            // Single line description
+	help    string            // Detailed and formated description
+	arg_min int               // Minimal argument number expected
+	arg_max int               // Maximal argument number expected
+	exec    fn (s []string) ! // Command callback.
 }
 
 // get is the main command access. It returns a complete list of all available commands
