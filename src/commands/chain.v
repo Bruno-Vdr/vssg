6186d4c71d5098a7973b70997d38e24e2ecefb31
Bruno <bruno@asterope.fr>
2025-02-28 10:22:51 +0100

    Implement chain link generation. Algorithm for validation if next/previous is Ok

--- src/commands/chain.v
+++ src/commands/chain.v
@@ -3,6 +3,8 @@ module commands
 import term
 import util
 import constants as cst
+import structures { Topic }
+import os
 
 // Chain structure, implementing Command interface.
 struct Chain implements Command {
@@ -43,8 +45,57 @@ Order is given by order of push in .topic file.
 }
 
 // chain command feature are implemented here. The parameters number has been checked before call.
-fn chain(p []string) ! {
+fn chain(params []string) ! {
 	if util.where_am_i() in [.blog_dir, .outside] {
 		return error('This command must be run from a topic directory.')
 	}
+
+	topics := Topic.load()!
+
+	if topics.posts.len == 0 {
+		return error('The topic "${topics.title}" does not contain any push.')
+	}
+	println('Chaining "${topics.title}" : ${topics.posts.len} pushes found.')
+
+	lst := topics.posts.values()
+	for id, ps in lst {
+
+		prev := if id == 0 {?int(none)} else {int(lst[id - 1].id)}
+		next := if id == lst.len -1 {?int(none)} else  {int(lst[id + 1].id)}
+
+		prev_lnk := generate_link(prev, .previous)
+		next_lnk := generate_link(next, .next)
+
+		println(ps.title)
+		println('Prev link= ${prev_lnk}')
+		println('Next link= ${next_lnk}')
+	}
+}
+enum LnkType {
+	next
+	previous
+}
+
+fn generate_link(to ?int, kind LnkType) string {
+
+	// Style is used as HTML On/Off button to show and hide the link.
+	href := if to != none {
+		'<a href="..${os.path_separator}${cst.push_dir_prefix}${to}${os.path_separator}${cst.push_filename}">Previous</a>'
+	} else {
+		'<a style="display: none;"></a>'
+	}
+
+	mut ln :=''
+	match kind {
+		.next {
+
+//<vssg-lnk-prev><a href="../push_2/index.html">Prev Push</a></vssg-lnk-prev>
+//<vssg-lnk-prev><a style="display: none;">Prev Push</a></vssg-lnk-prev>
+			ln = cst.lnk_next_tag + href + cst.next_tag_close
+		}
+		.previous {
+			ln = cst.lnk_prev_tag + href + cst.prev_tag_close
+		}
+	}
+	return ln
 }
