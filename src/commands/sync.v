4c2104aa9cd9692006481b29ee2e4e49a976de29
Bruno <bruno@asterope.fr>
2025-04-25 12:12:02 +0200

    Added -dry option to sync command. This prevent command to be executed, but it's still printed out.

--- src/commands/sync.v
+++ src/commands/sync.v
@@ -24,7 +24,7 @@ pub fn Sync.new() Command {
 		desc:    'Synchronizes the local blog with the remote blog. It means publish the blog.'
 		help:    Sync.help()
 		arg_min: 0
-		arg_max: 1
+		arg_max: 2
 		exec:    sync
 	}
 }
@@ -32,7 +32,7 @@ pub fn Sync.new() Command {
 // help give a complete description of the command, including parameters.
 fn Sync.help() string {
 	return '
-Command: ${term.green('vssg')} ${term.yellow('sync')} [option]
+Command: ${term.green('vssg')} ${term.yellow('sync')} [option] [-dry]
 
 The sync command performs a synchronization between locale and remote directories:
 Source is defined with the env var ${term.yellow(cst.blog_root)}  (set to: ${util.get_blog_root() or {
@@ -46,6 +46,8 @@ Options are appended to the command line:
 ${term.green('vssg')} ${term.yellow('sync')} abc with append abc to the default list of command.
 ${term.green('vssg')} ${term.yellow('sync')} " --delete --Xxxxx" will add detached options.
 
+The ${term.gray('-dry')} option prevents command execution, and only prints the rsync command.
+
 Permanent option can be set using environment variable ${term.yellow(cst.rsync_permanent_option)} e.g.  "-e \'ssh -p 2223\'" to specify
 a different SSH port with rsync under sync command.
 '
@@ -55,15 +57,32 @@ a different SSH port with rsync under sync command.
 // This operation is related to current directory in the blog. On blog's root, the full blog will be sync.
 // From within a directory, only this subdirectory and recursive will be synced.
 fn sync(p []string) ! {
-	options := if p.len == 1 {
-		if p[0].starts_with('-') {
-			' ' + p[0]
-		} else {
-			return error('Malformed option "${term.gray(p[0])}". Additional sync option must start with   "-". ')
+	dry := '-dry' in p
+	mut options := ''
+
+	if p.len == 2 {
+		if '-dry' !in p {
+			return error('Malformed option "${term.gray(p[0])}". Multi options must contain -dry and the other option. ')
 		}
-	} else {
-		''
+
+		// 2 param determine which is rsync opt (not -dry)
+		opt_ind := if p[0] == '-dry' { 1 } else { 0 }
+		options = if p[opt_ind].starts_with('-') {
+			' ' + p[opt_ind]
+		} else {
+			return error('Malformed option "${term.gray(p[opt_ind])}". Additional sync option must start with   "-". ')
+		}
 	}
+	if p.len == 1 {
+		if !dry {
+			options = if p[0].starts_with('-') {
+				' ' + p[0]
+			} else {
+				return error('Malformed option "${term.gray(p[0])}". Additional sync option must start with   "-". ')
+			}
+		}
+	}
+
 	url := util.get_remote_url() or {
 		return error('${cst.remote_url} environment variable not set.')
 	}
@@ -88,7 +107,11 @@ fn sync(p []string) ! {
 	// add n for dry run. On source, trailing '/' is required to sync the whole directory.
 	cmd := '${cst.rsync_cmd_opt} ${options} ${permanent_opt} ${cwd}${os.path_separator} ${url}${sub_dir}' //
 	println('${term.bright_yellow(cst.rsync_cmd_opt)} ${term.gray(options)} ${term.blue(permanent_opt)} ${cwd}${os.path_separator} ${url}${sub_dir}')
-	run_sync_cmd(cmd)!
+	if !dry {
+		run_sync_cmd(cmd)!
+	} else {
+		println('Command skipped (-dry) option.')
+	}
 	println('${msg} : Done.')
 }
 
