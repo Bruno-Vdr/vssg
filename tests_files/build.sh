6e321a03cc3a5fcef50976965468d217237690d8
Bruno <bruno@asterope.fr>
2025-05-05 11:29:54 +0200

    Cleaned up build.sh

--- tests_files/build.sh
+++ tests_files/build.sh
@@ -21,37 +21,7 @@ do
   vssg chain
   cd ..
 done
-#vssg add SolSys
-#vssg add Small
-#vssg add Third
-#
-## Fill Solsys topic
-#cd ./2dc8c707808d050a # cd SolSys
-#vssg push Cydonia.txt
-#vssg push Mercure.txt
-#vssg push Neptune.txt
-#vssg push Sedna.txt
-#vssg chain
-#cd ..
-#
-## Fill Small topic
-#cd ./3d2cc8d952adebec # cd Small
-#vssg push Sedna.txt
-#vssg push Sedna.txt
-#vssg chain
-#vssg bend ./push_1/index.htm
-#cd ..
-#
-## Fill Third topic
-#cd ./ce9a28d8e47beefe # cd Third
-#vssg push Jupiter.txt
-#vssg remove -f 0
-#vssg push Mercure.txt
-#vssg push Mercure.txt
-#vssg modify 1 Jupiter.txt
-#vssg chain
-#
-#cd ..
-#cd .. # Back out of Blog
+
+cd .. # Back out of Blog
 
 
