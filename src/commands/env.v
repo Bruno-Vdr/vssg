a3f833914f617077a7e32ac4d01e69fff66e6706
Bruno <bruno@asterope.fr>
2025-06-08 01:53:53 +0200

    Added the -e option on env command to emit export commands to set env var. Usefull to generate conf file for a blog.

--- src/commands/env.v
+++ src/commands/env.v
@@ -27,7 +27,7 @@ pub fn Env.new() Command {
 		desc:       'Displays environment variables used by vssg.'
 		help:       Env.help()
 		arg_min:    0
-		arg_max:    0
+		arg_max:    1
 		exec:       env
 	}
 }
@@ -35,7 +35,7 @@ pub fn Env.new() Command {
 // help give a complete description of the command, including parameters.
 fn Env.help() string {
 	return '
-Command: ${term.green('vssg')} ${term.yellow('env')}
+Command: ${term.green('vssg')} ${term.yellow('env')} ${term.gray('-e')}
 
 The env command displays the environment variables used by vssg:
 
@@ -45,51 +45,41 @@ The env command displays the environment variables used by vssg:
     ${term.bright_yellow(cst.remote_url)} : Remote blog\'s URL (used by sync command).
     ${term.bright_yellow(cst.blog_root)} : Local blog\' location (used by sync command).
     ${term.bright_yellow(cst.rsync_permanent_option)} : Permanent customizable rsync option e.g. "-e \'ssh -p 2223\'".
+
+Adding the  ${term.gray('-e')} option will emit a set of shell commands that exports these variables. Its can
+be used to generate a shell file that one can run, to switch from blog to another.
 '
 }
 
 // env command feature are implemented here. The parameters number has been checked before call.
 fn env(p []string, run_locked bool) ! {
-	println("vssg's environement variables:\n")
-	print(term.bright_yellow(cst.default_push_dir) + ' = ')
-	if push_dir := util.get_default_push_dir() {
-		println('"' + push_dir + '"')
+	mut export := false
+	println("# vssg's environment variables:\n")
+	if p.len == 1 && p[0] != '-e' {
+		return error('Unknown option "${p[0]}" for the env command.')
 	} else {
-		println(term.red('Not set'))
-	}
-	print(term.bright_yellow(cst.img_src_env) + ' = ')
-	if img_post := util.get_img_push_dir() {
-		println('"' + img_post + '"')
-	} else {
-		println(term.red('Not set'))
-	}
-	print(term.bright_yellow(cst.default_tmpl_dir) + ' = ')
-	if img_post := util.get_default_template_dir() {
-		println('"' + img_post + '"')
-	} else {
-		println(term.red('Not set'))
+		export = p.len == 1
 	}
 
-	print(term.bright_yellow(cst.remote_url) + ' = ')
-	if url := util.get_remote_url() {
-		println('"' + url + '"')
-	} else {
-		println(term.red('Not set'))
-	}
-
-	print(term.bright_yellow(cst.blog_root) + ' = ')
-	if url := util.get_blog_root() {
-		println('"' + url + '"')
-	} else {
-		println(term.red('Not set'))
-	}
-
-	print(term.bright_yellow(cst.rsync_permanent_option) + ' = ')
-	if url := util.get_sync_opt() {
-		println('"' + url + '"')
-	} else {
-		println(term.red('Not set'))
-	}
-
+	print_std(export, cst.default_push_dir, util.get_default_push_dir())
+	print_std(export, cst.img_src_env, util.get_img_push_dir())
+	print_std(export, cst.default_tmpl_dir, util.get_default_template_dir())
+	print_std(export, cst.remote_url, util.get_remote_url())
+	print_std(export, cst.blog_root, util.get_blog_root())
+	print_std(export, cst.rsync_permanent_option, util.get_sync_opt())
 	println('')
+}
+
+// print_std emits on stdout variables value, colored or not based on export value.
+fn print_std(export bool, var string, val ?string) {
+	if export {
+		value := if val == none {'Not set'} else {val}
+		println('export ${var}="${value}"')
+	} else {
+		if val != none {
+			println('${term.bright_yellow(var)}="${val}"')
+		} else {
+			println('${term.bright_yellow(var)}=${term.red('Not set')}')
+		}
+	}
 }
